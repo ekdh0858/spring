e0c517d6a7fc597ad8972dc94dbb3b886f5d0d85
DESKTOP-8Q6B06N\User <ekdh0858@naver.com>
2021-12-23 11:52:37 +0900

    -12/23 수업 내용  커밋

--- src/main/java/chapter06/config/AppCtx.java
+++ src/main/java/chapter06/config/AppCtx.java
@@ -12,13 +12,19 @@ import chapter06.aspecct.ExecTimeAspect;
 // 핵심 기능에 적용하기 위해서 설정 클래스에 필요한 애노테이션
 // 이 애노테이션을 추가해야 스프링 컨테이너가 이 애노테이션을 보고 공통 기능과 핵심 기능을 연결
 @Configuration
-@EnableAspectJAutoProxy
+// 인터페이스를 기반으로 프록시 객체를 만드는게 아니라
+// 클래스를 기반으로 프록시 객체를 만들고자 할 때는
+// @EnableAspectJAutoProxy 애노테이션에 proxyTargetclass = true 속성을 주면 됨
+
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AppCtx {
 	@Bean
 	public ExecTimeAspect execTimeAspect() {
 		return new ExecTimeAspect();
 	}
 	
+	// calculator 이름의 빈 객체 타입은 Calculator 타입
+	// 이 빈 객체의 실제 인스턴스 타입은 Impcalculator1 타입
 	@Bean
 	public Calculator calculator() {
 		return new ImpCalculator1();
